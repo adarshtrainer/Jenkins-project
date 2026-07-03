7fc80c9583734c005b1269be93fb8bc8d7140455
Adarsh-Teaching <147122925+adarshtrainer@users.noreply.github.com>
2026-07-03 09:30:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(`
-    <h1>Jenkins Node.js Demo</h1>
+    <h1>Jenkins webhook Node.js Demo</h1>
     <p>Build completed successfully from Jenkins Freestyle Job!</p>
     <p>Application is running on port ${PORT}</p>
   `);
